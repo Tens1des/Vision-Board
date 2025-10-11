89afa0c24bfd6530576a235df1784b7736bb52cf
Tens1des <romankotov3213@gmail.com>
2025-10-11 15:06:28 +0300

    Initial Commit

---
+++ BoardNote/BoardNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardNoteApp.swift
+//  BoardNote
+//
+//  Created by Рома Котов on 11.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BoardNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
